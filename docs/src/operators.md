21de51619028b887941dd653035949f809d9d956
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-06-06 15:52:28 +0200

    doc: Properly document `src/operators.jl`

---
+++ docs/src/operators.md
@@ -0,0 +1,6 @@
+# Expressions with creation/annihilation operators of fermions
+
+```@autodocs
+Modules = [KeldyshED.Operators]
+Order = [:module, :type, :function]
+```
--- src/operators.jl
+++ src/operators.jl
@@ -18,6 +18,10 @@
 #
 # Authors: Igor Krivenko, Hugo U.R. Strand
 
+"""
+This module implements an algebra of fermionic creation/annihilation operators
+and polynomial expressions built out of such operators.
+"""
 module Operators
 
 export IndicesType
@@ -25,13 +29,18 @@ export OperatorExpr, RealOperatorExpr, ComplexOperatorExpr, scalartype
 export c, c_dag, n, dagger
 
 using DataStructures
+using DocStringExtensions
 
 #################################
 # Auxiliary types and functions #
 #################################
 
-"""Combination of String/Int indices"""
-const IndicesType = Vector{Union{String,Int}}
+"""
+$(TYPEDEF)
+
+Compound operator index ``\\alpha`` as a mixed list of `String` / `Int` indices.
+"""
+const IndicesType = Vector{Union{String, Int}}
 
 function cmp(ind1::IndicesType, ind2::IndicesType)
   for (i1, i2) in zip(ind1, ind2)
@@ -47,10 +56,29 @@ function cmp(ind1::IndicesType, ind2::IndicesType)
   return cmp(length(ind1), length(ind2))
 end
 
-"""Canonical operator C/C^+"""
+"""
+This structure represents a single creation operator ``c^\\dagger_\\alpha`` or
+an annihilation operator ``c_\\alpha``.
+
+Fields
+------
+$(TYPEDFIELDS)
+
+Canonical operators are less-comparable and are ordered according to
+the following rule,
+
+``
+    c^\\dagger_\\alpha < c^\\dagger_\\beta < c^\\dagger_\\gamma <
+    c_\\gamma < c_\\beta < c_\\alpha,
+``
+
+where compound indices satisfy ``\\alpha < \\beta < \\gamma``.
+"""
 struct CanonicalOperator
-  dagger::Bool          # Creation/annihilation
-  indices::IndicesType  # Indices of canonical operator
+  "Creation operator if `true`, annihilation operator otherwise"
+  dagger::Bool
+  "Compound index α carried by the operator"
+  indices::IndicesType
 end
 
 function Base.:(==)(op1::CanonicalOperator, op2::CanonicalOperator)
@@ -68,13 +96,27 @@ function Base.isless(op1::CanonicalOperator, op2::CanonicalOperator)
   end
 end
 
-"""Hermitian conjugate"""
+"""
+$(TYPEDSIGNATURES)
+
+Hermitian conjugate of a canonical operator.
+"""
 dagger(op::CanonicalOperator) = CanonicalOperator(!op.dagger, op.indices)
 
+"""
+$(TYPEDSIGNATURES)
+
+Check if `op2` is a Hermitian conjugate of `op1`.
+"""
 function isconjugate(op1::CanonicalOperator, op2::CanonicalOperator)
   op1.dagger != op2.dagger && op1.indices == op2.indices
 end
 
+"""
+$(TYPEDSIGNATURES)
+
+Print a canonical operator.
+"""
 function Base.show(io::IO, op::CanonicalOperator)
   print(io, "c", (op.dagger ? "†" : ""), "(")
   print(io, join(map(repr, op.indices), ","), ")")
@@ -84,8 +126,18 @@ end
 # Monomial: Product of canonical operators #
 ############################################
 
-"""An ordered product of canonical operators"""
+"""
+An ordered product of canonical operators.
+
+Fields
+------
+$(TYPEDFIELDS)
+
+Monomials are less-comparable, with a shorter monomial being always considered
+lesser than a longer one.
+"""
 struct Monomial
+  "List of canonical operators in the product, left-to-right."
   ops::Vector{CanonicalOperator}
 end
 
@@ -101,14 +153,33 @@ function Base.isless(m1::Monomial, m2::Monomial)
   end
 end
 
+"""
+$(TYPEDSIGNATURES)
+
+Number of canonical operators in a given monomial.
+"""
 Base.length(m::Monomial) = length(m.ops)
+"""
+$(TYPEDSIGNATURES)
+
+Check if a given monomial is 1, i.e. a product of zero canonical operators.
+"""
 Base.isempty(m::Monomial) = imempty(m.ops)
 
-"""Hermitian conjugate"""
+"""
+$(TYPEDSIGNATURES)
+
+Hermitian conjugate of a monomial.
+"""
 function dagger(m::Monomial)
   Monomial([dagger(op) for op in Iterators.reverse(m.ops)])
 end
 
+"""
+$(TYPEDSIGNATURES)
+
+Print a monomial.
+"""
 function Base.show(io::IO, m::Monomial)
   if ! isempty(m.ops)
     print(io, "*", join(map(repr, m.ops)))
@@ -121,31 +192,93 @@ const const_monomial = Monomial()
 # OperatorExpr #
 ################
 
-"""Polynomial built out of canonical operators"""
+"""
+Polynomial expression built out of canonical operators and numeric coefficients,
+
+``
+M^{(0)} + \\sum_\\alpha M^{(1)}_\\alpha o_\\alpha +
+    \\sum_{\\alpha\\beta} M^{(2)}_{\\alpha\\beta} o_\\alpha o_\\beta +
+    \\sum_{\\alpha\\beta\\gamma} M^{(3)}_{\\alpha\\beta\\gamma}
+        o_\\alpha o_\\beta o_\\gamma + \\ldots,
+``
+
+where ``o_\\alpha`` are canonical operators ``c^\\dagger_\\alpha`` / ``c_\\alpha``,
+and ``M^{(n)}`` are the coefficients.
+
+Fields
+------
+$(TYPEDFIELDS)
+
+Polynomial expressions support the following arithmetic operations.
+
+- Addition / subtraction of two expressions with the same coefficient type.
+- Multiplication of two expressions with the same coefficient type.
+- Addition / subtraction of a constant.
+- Multiplication / division by a constant.
+- Unary minus.
+
+They also support the iteration interface with iteration element type being
+`Pair{Monomial, ScalarType}`.
+"""
 struct OperatorExpr{ScalarType <: Number}
-  # Monomial -> coefficient in front of it
+  "Sorted list of monomials with numeric coefficients ``M^{(n)}`` in front of them."
   monomials::SortedDict{Monomial, ScalarType}
 end
 
-# Construct empty expression
-OperatorExpr{S}() where S = OperatorExpr{S}(SortedDict{Monomial, S}())
-# Construct constant expression
+"""
+```julia
+OperatorExpr{S}() where S
+```
+
+Construct a polynomial expression with coefficient type `S` identically equal to 0.
+"""
+OperatorExpr{S}() where {S <: Number} = OperatorExpr{S}(SortedDict{Monomial, S}())
+"""
+```julia
+OperatorExpr{S}(x::S) where {S <: Number}
+```
+
+Construct a polynomial expression with coefficient type `S` identically equal to `x`.
+"""
 function OperatorExpr{S}(x::S) where {S <: Number}
   OperatorExpr{S}(SortedDict{Monomial, S}(Monomial() => x))
 end
 
+"""
+$(TYPEDEF)
+
+Polynomial expression with real coefficients.
+"""
 const RealOperatorExpr = OperatorExpr{Float64}
+"""
+$(TYPEDEF)
+
+Polynomial expression with complex coefficients.
+"""
 const ComplexOperatorExpr = OperatorExpr{ComplexF64}
 
 ##################################
 # OperatorExpr: basic operations #
 ##################################
 
-"""Determine the type of operator monomial coefficients"""
+"""
+$(TYPEDSIGNATURES)
+
+Determine the type of monomial coefficients of a given polynomial expression.
+"""
 scalartype(::OperatorExpr{S}) where S = S
+"""
+$(TYPEDSIGNATURES)
+
+Determine the type of monomial coefficients of a given polynomial expression type.
+"""
 scalartype(::Type{OperatorExpr{S}}) where S = S
 
-# Is zero operator?
+"""
+$(TYPEDSIGNATURES)
+
+Check if a given polynomial expression is identically zero.
+"""
 Base.iszero(op::OperatorExpr{S}) where S = isempty(op.monomials)
 
 function Base.:(==)(op1::OperatorExpr{S}, op2::OperatorExpr{S}) where S
@@ -288,7 +421,11 @@ function normalize_and_insert(m::Monomial,
   end
 end
 
-"""Hermitian conjugate"""
+"""
+$(TYPEDSIGNATURES)
+
+Hermitian conjugate of a polynomial expression.
+"""
 function dagger(op::OperatorExpr{S}) where S
   monomials = SortedDict{Monomial, S}()
   for (m, c) in op.monomials
@@ -302,7 +439,17 @@ end
 #####################################
 Base.eltype(op::OperatorExpr{S}) where S = Pair{Monomial, S}
 
+"""
+$(TYPEDSIGNATURES)
+
+Number of monomials in a given polynomial expression `op`.
+"""
 Base.length(op::OperatorExpr) = length(op.monomials)
+"""
+$(TYPEDSIGNATURES)
+
+Check if a given polynomial expression is identically zero.
+"""
 Base.isempty(op::OperatorExpr) = length(op) == 0
 
 Base.iterate(op::OperatorExpr) = iterate(op.monomials)
@@ -311,6 +458,15 @@ Base.iterate(op::OperatorExpr, it) = iterate(op.monomials, it)
 #############################################
 # OperatorExpr: map() and related functions #
 #############################################
+
+"""
+$(TYPEDSIGNATURES)
+
+Transform a given polynomial expression `op` by applying a function `f`
+to coefficients of all monomials in the expression. If `f` applied
+to a coefficient returns zero, the corresponding monomial is omitted
+from the resulting expression.
+"""
 function Base.map(f, op::OperatorExpr{S}) where S
   res = OperatorExpr{S}()
   for (m, c) in op
@@ -322,13 +478,30 @@ function Base.map(f, op::OperatorExpr{S}) where S
   res
 end
 
+"""
+$(TYPEDSIGNATURES)
+
+Return a version of a polynomial expression `op` with all coefficients replaced by
+their real parts.
+"""
 Base.real(op::OperatorExpr) = map((m, c) -> real(c), op)
+"""
+$(TYPEDSIGNATURES)
+
+Return a version of a polynomial expression `op` with all coefficients replaced by
+their imaginary parts.
+"""
 Base.imag(op::OperatorExpr) = map((m, c) -> imag(c), op)
 
 #######################################
 # OperatorExpr: String representation #
 #######################################
 
+"""
+$(TYPEDSIGNATURES)
+
+Print a polynomial expression.
+"""
 function Base.show(io::IO, op::OperatorExpr{S}) where S
   if isempty(op.monomials)
     print(io, zero(S))
@@ -343,21 +516,42 @@ end
 # OperatorExpr: Factory functions #
 ###################################
 
-"""Make an annihilation operator"""
+"""
+$(TYPEDSIGNATURES)
+
+Make an annihilation operator ``c_\\alpha`` with `indices...` being
+components of the compound index ``\\alpha``. Coefficient type
+of the resulting polynomial expression can be specified via
+`scalar_type` (defaults to `Float64`).
+"""
 function c(indices...; scalar_type = Float64)
   m = Monomial([CanonicalOperator(false, collect(indices))])
   OperatorExpr{scalar_type}(
     SortedDict{Monomial,scalar_type}(m => one(scalar_type)))
 end
 
-"""Make a creation operator"""
+"""
+$(TYPEDSIGNATURES)
+
+Make an creation operator ``c^\\dagger_\\alpha`` with `indices...` being
+components of the compound index ``\\alpha``. Coefficient type
+of the resulting polynomial expression can be specified via
+`scalar_type` (defaults to `Float64`).
+"""
 function c_dag(indices...; scalar_type = Float64)
   m = Monomial([CanonicalOperator(true, collect(indices))])
   OperatorExpr{scalar_type}(
     SortedDict{Monomial,scalar_type}(m => one(scalar_type)))
 end
 
-"""Make a particle number operator"""
+"""
+$(TYPEDSIGNATURES)
+
+Make a particle number operator ``n_\\alpha = c^\\dagger_\\alpha c_\\alpha``
+with `indices...` being components of the compound index ``\\alpha``.
+Coefficient type of the resulting polynomial expression can be specified via
+`scalar_type` (defaults to `Float64`).
+"""
 function n(indices...; scalar_type = Float64)
   m = Monomial([CanonicalOperator(true, collect(indices)),
                 CanonicalOperator(false, collect(indices))])
