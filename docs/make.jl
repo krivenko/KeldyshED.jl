ac65499f5dc5df621d21bbd3be117a5b30f211e2
Igor Krivenko <igor.s.krivenko@gmail.com>
2024-01-17 14:49:53 +0100

    docs: Pass `repo=Documenter.Remotes.GitHub(...)` to `makedocs()`

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,23 @@
+# KeldyshED.jl
+#
+# Copyright (C) 2019-2024 Igor Krivenko <igor.s.krivenko@gmail.com>
+# Copyright (C) 2015 P. Seth, I. Krivenko, M. Ferrero and O. Parcollet
+#
+# KeldyshED.jl is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# KeldyshED.jl is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# KeldyshED.jl. If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Igor Krivenko
+
 using KeldyshED
 using Documenter
 
@@ -6,7 +26,7 @@ makedocs(;
       KeldyshED,
       ],
     authors="Igor Krivenko <igor.s.krivenko@gmail.com>",
-    repo="https://github.com/krivenko/KeldyshED.jl/blob/{commit}{path}#L{line}",
+    repo=Documenter.Remotes.GitHub("krivenko", "KeldyshED.jl"),
     sitename="KeldyshED.jl",
     format=Documenter.HTML(;
         canonical = "https://krivenko.github.io/KeldyshED.jl/",
